be42f3c6c132a2bed13f6a43ff986dc36db2de22
ZacharyIanMay <zacharymay12383@gmail.com>
2022-02-25 01:29:08 -0800

    The seem to talk Need to resolve EC2 missing Node_modules

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ app.get("/test", (req, res) => {
     res.json({message: "Testing connection"});
 })
 
+//These two should be at the end, this is how we handle non-api calls. This sends the built React page
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
