ad29faa12992790687f7b229c8041e6dd53bf319
ZacharyIanMay <zacharymay12383@gmail.com>
2022-02-25 01:23:38 -0800

    EC2 was complaining that node_modules didn't exist Hate to add so much to the repo, but I don't know a way around

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
+const path = require('path');
 const express = require("express");
 const PORT = 3001;
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.get("/test", (req, res) => {
     res.json({message: "Testing connection"});
 })
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server Listening on ${PORT}`);
